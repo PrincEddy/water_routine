9d968f77be1e9f506577c48b8e8c44ecaba33945
PrincEddy <mukoeddy8@gmail.com>
2019-07-14 15:55:18 +0800

    created water routine project

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(WaterRoutine());
+
+class WaterRoutine extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
